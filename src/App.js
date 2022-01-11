21b63d25864912cc9560aedf671d488d51a2ef28
Yordan <yordan_yordanov001@abv.bg>
2022-01-11 21:08:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-import Title from "./Title";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increseAndLog = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <section class="hero">
@@ -12,7 +17,9 @@ function App() {
       </section>
       <div class="container is-fullhd">
         <div class="notification">
-          <Title />
+          <button type="button" onClick={increseAndLog}>
+            Count {count}
+          </button>
         </div>
       </div>
     </div>
